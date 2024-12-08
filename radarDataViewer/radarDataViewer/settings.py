61712e4cf3cd23daf476b92f1d9bf231441ac365
B M Al-amin Badhon <alamin.badhonn@gmail.com>
2024-12-08 17:16:16 -0330

    solved env problem

--- radarDataViewer/radarDataViewer/settings.py
+++ radarDataViewer/radarDataViewer/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#w=7qelmxxt%n(+e+%@+-k8p&q#m8w9ma4_v5a$%5blx#clthv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
